e0606a70347ff989d113932b70d4c08d533cf769
Jaydeep Khokhar <52407335+jaydeeppkhokhar@users.noreply.github.com>
2020-05-28 13:18:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ rm ~/.config/google-chrome/Default/Extensions/gidgenkbbabolejbgbpnhbimgjbffefm/1
 wget "https://raw.githubusercontent.com/theapache64/vysor-pro-hack/master/uglify.js" &&
 
 # Moving to real position
-mv uglify.js ~/.config/google-chrome/Default/Extensions/gidgenkbbabolejbgbpnhbimgjbffefm/1.9.3_0/ &&
+mv uglify.js ~/.config/google-chrome/Default/Extensions/gidgenkbbabolejbgbpnhbimgjbffefm/2.3.0_0/ &&
 
 echo "########################################"
 echo "Done!! Please restart vysor to take effect"
